cf66d6d28200e25139c9010acda2202494198652
Shahram Sabzevari <sh.sabzevari@gmail.com>
2021-07-20 21:22:30 -0300

    Adding PCA and TSNE comparison of the data

--- experiment.py
+++ experiment.py
@@ -11,7 +11,8 @@ import requests as req
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
-
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
 
 def real_data_loading(data: np.array, seq_len):
     # Flip the data to make chronological data
@@ -116,3 +117,66 @@ if __name__ == '__main__':
                 secondary_y='Synthetic data', style=['-', '--'])
     fig.tight_layout()
     plt.savefig(os.path.join(os.path.dirname(__file__), 'graphs', 'data_comparison.png'))
+
+    sample_size = 250
+    idx = np.random.permutation(len(stock_data))[:sample_size]
+
+    real_sample = np.asarray(stock_data)[idx]
+    synthetic_sample = np.asarray(synth_data)[idx]
+
+    # for the purpose of comparision we need the data to be 2-Dimensional. For that reason we are going to use only two componentes for both the PCA and TSNE.
+    synth_data_reduced = real_sample.reshape(-1, seq_len)
+    stock_data_reduced = np.asarray(synthetic_sample).reshape(-1, seq_len)
+
+    n_components = 2
+    pca = PCA(n_components=n_components)
+    tsne = TSNE(n_components=n_components, n_iter=300)
+
+    # The fit of the methods must be done only using the real sequential data
+    pca.fit(stock_data_reduced)
+
+    pca_real = pd.DataFrame(pca.transform(stock_data_reduced))
+    pca_synth = pd.DataFrame(pca.transform(synth_data_reduced))
+
+    data_reduced = np.concatenate((stock_data_reduced, synth_data_reduced), axis=0)
+    tsne_results = pd.DataFrame(tsne.fit_transform(data_reduced))
+
+    # The scatter plots for PCA and TSNE methods
+    import matplotlib.gridspec as gridspec
+
+    fig = plt.figure(constrained_layout=True, figsize=(20, 10))
+    spec = gridspec.GridSpec(ncols=2, nrows=1, figure=fig)
+
+    # TSNE scatter plot
+    ax = fig.add_subplot(spec[0, 0])
+    ax.set_title('PCA results',
+                 fontsize=20,
+                 color='red',
+                 pad=10)
+
+    # PCA scatter plot
+    plt.scatter(pca_real.iloc[:, 0].values, pca_real.iloc[:, 1].values,
+                c='black', alpha=0.2, label='Original')
+    plt.scatter(pca_synth.iloc[:, 0], pca_synth.iloc[:, 1],
+                c='red', alpha=0.2, label='Synthetic')
+    ax.legend()
+
+    ax2 = fig.add_subplot(spec[0, 1])
+    ax2.set_title('TSNE results',
+                  fontsize=20,
+                  color='red',
+                  pad=10)
+
+    plt.scatter(tsne_results.iloc[:sample_size, 0].values, tsne_results.iloc[:sample_size, 1].values,
+                c='black', alpha=0.2, label='Original')
+    plt.scatter(tsne_results.iloc[sample_size:, 0], tsne_results.iloc[sample_size:, 1],
+                c='red', alpha=0.2, label='Synthetic')
+
+    ax2.legend()
+
+    fig.suptitle('Validating synthetic vs real data diversity and distributions',
+                 fontsize=16,
+                 color='grey')
+
+    # fig.tight_layout()
+    plt.savefig(os.path.join(os.path.dirname(__file__), 'graphs', 'synthetic_vs_real.png'))
